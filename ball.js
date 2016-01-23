9a799b4d56b06640cc1c7258c219a95e0e2adab5
Stephen Charaton <charaton@users.noreply.github.com>
2016-01-23 13:49:38 -0800

    Reimplement ball as class. Add check for ballType (defaults to 'regular') (only when creating new object).

--- ball.js
+++ ball.js
@@ -1,3 +1,9 @@
-var ball = {
-  ballType: "regular"
-};
+class Ball {
+  constructor(ballType) {
+    if (ballType !== "regular" && ballType !== "super") {
+      this.ballType = "regular";
+    } else {
+      this.ballType = ballType;
+    }
+  }
+}
